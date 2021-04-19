481ead2acdb9bececbc18dda9e82363c0bbb46f7
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-04-20 02:24:18 +0900

    各画面の繋ぎ込みとホーム画面作り込み

--- personal_tree/home_app/urls.py
+++ personal_tree/home_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home,github,qiita,youtube,twitter,hobby,portfolio
 
 urlpatterns = [
-    path('', home.main_func, name="main"),
+    path('', home.main_func, name="home"),
+    path('github', github.main_func, name="github"),
+    path('qiita', qiita.main_func, name="qiita"),
+    path('youtube', youtube.main_func, name="youtube"),
+    path('twitter', twitter.main_func, name="twitter"),
+    path('hobby', hobby.main_func, name="hobby"),
+    path('portfolio', portfolio.main_func, name="portfolio"),    
 ]
---
+++ personal_tree/home_app/views/twitter.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+
+def main_func(request):
+       
+    params = {
+        'add_image_bottom':'新規追加',
+    }
+
+    return render(request,'twitter.html',params)
