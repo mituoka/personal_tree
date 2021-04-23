b4b3c04a47dd04551d789ac5fd00d645575d1a67
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-04-23 14:05:02 +0900

    list表示調整

--- personal_tree/home_app/views/github.py
+++ personal_tree/home_app/views/github.py
@@ -10,21 +10,13 @@ def main_func(request):
         'repository_description':[],
     }
 
-    token = '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'
-
+    token = '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'
     # or using an access token
     g = Github(token)
     
-    git_link='a'
     for repo in g.get_user().get_repos(type='owner'):
         params['repository_list'].append(repo.name)
         params['repository_link'].append("https://github.com/mituoka/"+repo.name)
-        if repo.name == 'Convenient_tool':
-
-            print(repo.git_url)
-            print(repo.url)
-            print(repo.pulls_url)
-            print(repo.downloads_url)
         params['repository_description'].append(repo.description)
 
   
--- personal_tree/home_app/views/youtube.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-
-def main_func(request):
-       
-    params = {
-        'add_image_bottom':'新規追加',
-    }
-
-    return render(request,'youtube.html',params)
